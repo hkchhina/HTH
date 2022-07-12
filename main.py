57973e2b3c2ea328f37e4d917494067f4b881d78
harpreet kaur <>
2022-07-12 18:20:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+full_name = "Harpreet Kaur Chhina"
+nickname = "Harpreet"
+age = 18 
+has_used_python = False
+
+hobbies  = [
+  "reading"
+  "traveling"
+  "hanging out with my dog"
+  "watching kdramas"
+]
+fav_things = {
+  "kdrama" : "Kingdom"
+  "food" : "pizza"
+  "hobby" : 
+hobbies[0]
+"show/anime"
+"Paradise Kiss"
+}
+
+print(
+  f"Hello World! My name is {full name} , but many of my friends call me {nickname}. I am {age} years old."
+)
+print()
+print (f "Has Pyhton Experience: {has_used_python}")
+
+
+for key in fav_things.keys( ): print (f"My favorite {key} is {fav_things[key]}. "")
+
+                                      print()
+print (f"Here are some of my hobbies: {hobbies}"")  
+print()  
+
+all_vars = 
+                                    dict(vars())
+                                      
